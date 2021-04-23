38fc4f871679046ba295fa5e1bceb5114a3199ee
BusieDeveloper <brady@getbusie.com>
2021-04-23 16:58:41 -0400

    added some logging

--- random_password_generator/__init__.py
+++ random_password_generator/__init__.py
@@ -2,12 +2,15 @@
 import random
 import secrets
 import string
+import logging
 
 
 UPPER = string.ascii_uppercase
 LOWER = string.ascii_lowercase
 NUMBERS = string.digits
 SPECIAL = string.punctuation
+
+logger = logging.getLogger('random_password_generator')
 
 def generate_random_password(
     length:int=8,
@@ -20,15 +23,19 @@ def generate_random_password(
     """
     required_charsets = set()
     if numbers:
+        warn('using numbers in random password is recommended')
         required_charsets.add(NUMBERS)
     
     if special:
+        warn('using special characters in random password is recommended')
         required_charsets.add(SPECIAL)
     
     if uppercase:
+        warn('using uppercase letters in random password is recommended')
         required_charsets.add(UPPER)
     
     if lowercase:
+        warn('using lowercase letters in random password is recommended')
         required_charsets.add(LOWER)
 
     if not len(required_charsets):
@@ -43,8 +50,11 @@ def generate_random_password(
     if length not in range(8, 26):
         raise ValueError('`length` must be betweent 8 and 25')
     
-    _length = secrets.choice(range(length, 25)) - len(password)
+    _length = secrets.choice(range(length, 26)) - len(password)
     password += ''.join((secrets.choice(all_chars) for _ in range(_length)))
     password_list = list(password)
     random.shuffle(password_list)
     return ''.join(password_list)
+
+def warn(msg, *args):
+    logger.warning(msg, *args)
